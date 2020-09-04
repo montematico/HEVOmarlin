96796c73a61bc99d40e53af7777b421342160611
monte <montematico@gmail.com>
2020-09-04 12:45:10 -0400

    created cutom settings for TFT35

--- BIGTREETECH-TouchScreenFirmware/TFT/src/User/Menu/More.h
+++
@@ -1,6 +0,0 @@
-#ifndef _MORE_H_
-#define _MORE_H_
-
-void menuMore(void);
-
-#endif
--- BIGTREETECH-TouchScreenFirmware/TFT/src/User/Menu/Print.h
+++
@@ -1,9 +0,0 @@
-#ifndef _PRINT_H_
-#define _PRINT_H_
-#include "includes.h"
-
-void menuPrintFromSource(void);
-void menuPrint(void);
-bool get_Pre_Icon(void);
-
-#endif
